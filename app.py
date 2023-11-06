376c7da95df76345a39c1a91d9ce74840c6a6eba
mostghoste <sarunas.jacikas+github@gmail.com>
2023-11-06 14:15:23 +0200

    Added test transmission code

--- app.py
+++ app.py
@@ -1,15 +1,24 @@
 from flask import Flask, request, jsonify
 from gpiozero import LED
+from rpi_rf import RFDevice
+
 app = Flask(__name__)
 led = LED(15)
+transmitter = RFDevice(14)
+
+def send_signal(code):
+    transmitter.tx_code(code, 1, 350)
+    print(f'Signal {code} sent!')
 
 @app.route('/process/<int:number>', methods=['GET'])
 def process_number(number):
    if number == 1:
       led.on()
+      send_signal("TestCode1")
       print("LED turned ON")
    elif number == 2:
       led.off()
+      send_signal("TestCode2")
       print("LED turned OFF")
    elif number == 3:
       print("Number is Three")
